27edf13f56c29ee19b41e8722c5bd1cc2e22bf10
yichiang <yi_chiang@outlook.com>
2016-07-31 17:06:11 -0700

    add code

---
+++ scripts.js
@@ -0,0 +1,37 @@
+
+function spinalCase(str) {
+  // "It's such a fine line between stupid, and clever."
+  // --David St. Hubbins
+  str=str.replace(/\W/g,"-");
+  str=str.replace(/" "/g,"-");
+  str=str.replace(/[_]/g,"");
+
+    var result=str.match(/[A-Z]/g);
+    var temp=[];
+    for (var i = 0; i < str.length; i++) {
+      var foundtemp=false;
+      for (var j = 0; j < result.length; j++) {
+        if(str[i]===result[j]&&str[i-1]!=="-")
+        {
+
+          foundtemp=true;
+        }
+      }
+      if(foundtemp===true)
+        {
+                    temp.push(" "+str[i]);
+
+        }else {
+                    temp.push(str[i]);
+
+        }
+    }
+
+      str=temp.join("").trim().toLowerCase();
+  str=str.replace(/\s/g,"-");
+
+
+  return str;
+}
+
+spinalCase("AllThe-small Things");
